7ecf9ee178c59c1f4f18bbaa347559f214cd76d5
yuanyi <imtudous@163.com>
2021-01-22 18:08:54 +0800

    redishelper

--- RedisHelper/RedisHelper.Sample/RedisHelper.cs
+++ RedisHelper/RedisHelper.Sample/RedisHelper.cs
@@ -1,10 +1,11 @@
-﻿using StackExchange.Redis;
+﻿using Newtonsoft.Json;
+
+using StackExchange.Redis;
 
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Text.Json;
 
 namespace RedisHelper.Sample
 {
@@ -33,19 +34,53 @@ namespace RedisHelper.Sample
         /// <summary>
         /// 保存单个Key Value
         /// </summary>
-        /// <param name="key"></param>
-        /// <param name="value"></param>
-        /// <param name="expires"></param>
+        /// <param name="key">Redis Key</param>
+        /// <param name="value">保存的值</param>
+        /// <param name="expires">过期时间</param>
         /// <returns></returns>
         public bool StringSet(string key, string value,TimeSpan? expires = default)
         {
             return Database.StringSet(AddSysCustomKey(key), value, expires);
         }
 
+        /// <summary>
+        /// 保存多个Key Value
+        /// </summary>
+        /// <param name="keyValuePair">键值对</param>
+        /// <returns></returns>
         public bool StringSet(List<KeyValuePair<RedisKey, RedisValue>> keyValuePair)
         {
-            //var newKeyValueList = keyValuePair.Select(s=>s.) 
-            return false;
+            var newKeyValueList = keyValuePair.Select(s => new KeyValuePair<RedisKey, RedisValue>(AddSysCustomKey(s.Key), s.Value)).ToList();
+            return Database.StringSet(newKeyValueList.ToArray());
+        }
+
+        /// <summary>
+        /// 保存单个对象
+        /// </summary>
+        /// <typeparam name="T">obj</typeparam>
+        /// <param name="Key">Redis Key</param>
+        /// <param name="value">保存的值</param>
+        /// <param name="expires">过期时间</param>
+        /// <returns></returns>
+        public bool StringSet<T>(string Key,T value, TimeSpan? expires = default)
+        {
+            Key = AddSysCustomKey(Key);
+            return Database.StringSet(Key, value.ToJson(), expires);
+        }
+
+        /// <summary>
+        /// 获取单个对象
+        /// </summary>
+        /// <param name="Key"></param>
+        /// <returns></returns>
+        public string StringGet(string Key)
+        {
+            Key = AddSysCustomKey(Key);
+            return Database.StringGet(Key);
+        }
+
+        public RedisValue[] StringGet(List<string> Key)
+        { 
         
         }
 
@@ -55,12 +90,22 @@ namespace RedisHelper.Sample
             var prefixKey = CustomKey ?? RedisConnectionHelper.SysCustomKey;
             return prefixKey + oldKey;
         }
-
-        private string ConvertToJson<T>(T value)
-        {
-            return value is string ? value.ToString() : JsonSerializer.Serialize<T>(value);
-        } 
         #endregion
+
+    }
+
+    public static class RedisHelperExtension
+    {
+        public static string ToJson<T>(this T t)
+        {
+            return t is string ? t.ToString() : JsonConvert.SerializeObject(t);
+        }
+
+        public string AddSysCustomKey( this string oldKey)
+        {
+            var prefixKey = CustomKey ?? RedisConnectionHelper.SysCustomKey;
+            return prefixKey + oldKey;
+        }
 
     }
 }
