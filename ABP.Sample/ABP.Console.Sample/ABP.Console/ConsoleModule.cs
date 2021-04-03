3688b6c6c95c22ee0c631368a2467fd76bcddd41
yuanyi <imtudous@163.com>
2021-04-03 15:07:29 +0800

    abp sample

---
+++ ABP.Sample/ABP.Console.Sample/ABP.Console/ConsoleModule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+
+using Volo.Abp.Modularity;
+
+namespace ABP.Console
+{
+    public class ConsoleModule:AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            context.Services.AddTransient<HelloAbpService>();
+            base.ConfigureServices(context);
+        }
+    }
+}
---
+++ ABP.Sample/ABP.Console.Sample/ABP.Console/HelloAbpService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ABP.Console
+{
+    public class HelloAbpService
+    {
+        public void SayHello()
+        {
+            System.Console.WriteLine("Hello Abp!");
+        }
+    }
+}
