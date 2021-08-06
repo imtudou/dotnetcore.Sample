166761b8950701760923cec1896f4632c02c2446
yuanyi <imtudous@163.com>
2021-08-06 18:41:34 +0800

    CLR update

---
+++ CLR/CLR.Sample1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CLR.Sample1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Main Thread Start .....");
+            ThreadPool.QueueUserWorkItem(Sample1.ComputeBoundOp, 5);
+            Console.WriteLine("Child Thread DoSomething .....");
+            Thread.Sleep(1000);
+            Console.WriteLine("Main Thread End .....");
+
+            /*
+              output
+
+                    Main Thread Start .....
+                    Child Thread DoSomething .....
+                    In ComputeBoundOp:5
+                    Main Thread End .....
+                
+                or
+
+             
+              
+             */
+
+
+
+
+            // Sample2.ExecutionContextMethod(); 
+            // output
+            /*
+                  name = 
+                  name = zhangsan 
+             */
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
